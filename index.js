ae5ea6bed404ff72e7eedeb7d8451f1da8288246
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-27 05:52:21 +0900

    Fix wrong binary path
    Close #7

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ var path = require('path');
  * Module exports
  */
 
-exports.path = path.resolve('vendor', process.platform === 'win32' ? 'cwebp.exe' : 'cwebp');
+exports.path = path.join(__dirname, 'vendor', process.platform === 'win32' ? 'cwebp.exe' : 'cwebp');
 exports.version = '0.4.2';
