1eea030f0a71aa2a17a52a08a3b07fd2cc45ec35
Guilherme Nagatomo <gnagatomo@uolinc.com>
2018-07-12 09:49:55 -0300

    Fix code style

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 'use strict';
-const spawn = require('child_process').spawn;
+const {spawn} = require('child_process');
 const binPath = require('.');
 
 spawn(binPath, process.argv.slice(2), {stdio: 'inherit'})
