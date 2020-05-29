cb9145f416dc5ce3397e757d4ca5050a850cc917
Shogo Sensui <shogosensui@gmail.com>
2020-05-30 01:17:36 +0900

    Bump dependencies (#42)

--- test/test.js
+++ test/test.js
@@ -10,15 +10,15 @@ const compareSize = require('compare-size');
 const cwebp = require('..');
 
 test('rebuild the cwebp binaries', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 
 	await binBuild
 		.file(path.resolve(__dirname, '../vendor/source/libwebp-1.0.2.tar.gz'), [
-			`./configure --disable-shared --prefix="${tmp}" --bindir="${tmp}"`,
+			`./configure --disable-shared --prefix="${temporary}" --bindir="${temporary}"`,
 			'make && make install'
 		]);
 
-	t.true(fs.existsSync(path.join(tmp, 'cwebp')));
+	t.true(fs.existsSync(path.join(temporary, 'cwebp')));
 });
 
 test('return path to binary and verify that it is working', async t => {
@@ -26,9 +26,9 @@ test('return path to binary and verify that it is working', async t => {
 });
 
 test('minify and convert a PNG to WebP', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 	const src = path.join(__dirname, 'fixtures/test.png');
-	const dest = path.join(tmp, 'test-png.webp');
+	const dest = path.join(temporary, 'test-png.webp');
 	const args = [
 		src,
 		'-o',
@@ -36,15 +36,15 @@ test('minify and convert a PNG to WebP', async t => {
 	];
 
 	await execa(cwebp, args);
-	const res = await compareSize(src, dest);
+	const result = await compareSize(src, dest);
 
-	t.true(res[dest] < res[src]);
+	t.true(result[dest] < result[src]);
 });
 
 test('minify and convert a JPG to WebP', async t => {
-	const tmp = tempy.directory();
+	const temporary = tempy.directory();
 	const src = path.join(__dirname, 'fixtures/test.jpg');
-	const dest = path.join(tmp, 'test-jpg.webp');
+	const dest = path.join(temporary, 'test-jpg.webp');
 	const args = [
 		src,
 		'-o',
@@ -52,7 +52,7 @@ test('minify and convert a JPG to WebP', async t => {
 	];
 
 	await execa(cwebp, args);
-	const res = await compareSize(src, dest);
+	const result = await compareSize(src, dest);
 
-	t.true(res[dest] < res[src]);
+	t.true(result[dest] < result[src]);
 });
