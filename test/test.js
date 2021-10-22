fa220e6dfe77164257fdde3fcb5f0e8c75016082
1000ch <shogosensui@gmail.com>
2021-10-22 23:08:57 +0900

    Enable tests on Windows

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 import test from 'ava';
 import execa from 'execa';
@@ -10,6 +11,12 @@ import compareSize from 'compare-size';
 import cwebp from '../index.js';
 
 test('rebuild the cwebp binaries', async t => {
+	// Skip the test on Windows
+	if (process.platform === 'win32') {
+		t.pass();
+		return;
+	}
+
 	const temporary = tempy.directory();
 	const source = fileURLToPath(new URL('../vendor/source/libwebp-1.1.0.tar.gz', import.meta.url));
 
